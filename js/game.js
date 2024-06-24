c8f185a8190e44098b80fff7d53ad56e471e3d68
Mario Markwart <mario.markwart@gmail.com>
2024-06-24 08:50:14 +0200

    07 -> create classes

--- js/game.js
+++ js/game.js
@@ -2,9 +2,14 @@ let canvas;
 let ctx;
 
 
+let world = new World();
+
+
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
+    console.log("My world is: ",  world);
+    console.log("My character is: ",  world.character);
 
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,6 +1,6 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 400;
     img;
 
     constructor(x, y, img){
@@ -8,9 +8,15 @@ class MovableObject {
         this.y = y;
         this.img = img;
     }
+
     moveRight(){
         console.log('Moving right');
         this.x += 10;
         ctx.drawImage(this.img, this.x, this.y, 50, 150);
+    }
+
+    
+    moveLeft(){
+        console.log('Moving left');
     }
 }
