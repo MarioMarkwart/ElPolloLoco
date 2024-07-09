152a104c5546f0fadb8d1dc872333b2e9feb1b32
Mario Markwart <mario.markwart@gmail.com>
2024-07-09 12:19:31 +0200

    world -> added background images up to 2157px

--- models/world.class.js
+++ models/world.class.js
@@ -12,12 +12,31 @@ class World{
     ];
 
     backgroundObjects = [
+        new BackgroundObject('../assets/img/5_background/layers/air.png',-1438),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/1.png',-1438),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/1.png',-1438),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/1.png', -1438),
+        new BackgroundObject('../assets/img/5_background/layers/air.png',-719),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png',-719),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png',-719),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', -719),
         new BackgroundObject('../assets/img/5_background/layers/air.png'),
         new BackgroundObject('../assets/img/5_background/layers/3_third_layer/1.png'),
         new BackgroundObject('../assets/img/5_background/layers/2_second_layer/1.png'),
         new BackgroundObject('../assets/img/5_background/layers/1_first_layer/1.png'),
         new BackgroundObject('../assets/img/5_background/layers/air.png',719),
-
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png',719),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png',719),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', 719),
+        new BackgroundObject('../assets/img/5_background/layers/air.png',1438),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/1.png',1438),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/1.png',1438),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/1.png', 1438),
+        new BackgroundObject('../assets/img/5_background/layers/air.png',2157),
+        new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png',2157),
+        new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png',2157),
+        new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', 2157),
+        
     ];
 
     canvas;
