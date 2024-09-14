9a17f44d98d5f60256a9e4c79d2353159e63868f
Mario Markwart <mario.markwart@gmail.com>
2024-09-14 17:33:28 +0200

    bottle splash on ground

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,5 +1,6 @@
 class ThrowableObject extends MovableObject {
 	throwingAnimationId = null;
+	moveToInterval = null;
 
 	IMAGES_BOTTLE_ROTATION = [
 		"assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
@@ -36,13 +37,22 @@ class ThrowableObject extends MovableObject {
 		);
 		this.speedY = 10;
 		this.applyGravity();
-		this.intervalId = setInterval(() => {
+		this.moveToInterval = setInterval(() => {
 			this.x += 10;
+			if(this.y > 360) {
+				this.speedY = 0;
+				this.bottleSplash();
+				clearInterval(this.moveToInterval);
+			}
 		}, 25);
 	}
 
 	bottleSplash() {
 		clearInterval(this.throwingAnimationId);
-		this.animate(this.IMAGES_BOTTLE_SPLASH, 50, 1);
-	}
+		this.animate(this.IMAGES_BOTTLE_SPLASH, 50);
+		setTimeout(() => {
+			this.height = 0;
+			this.width = 0;
+		},200)
+ 	}
 }
