2865a41db58c4f4aecfc762b9262a1626242d98b
Mario Markwart <mario.markwart@gmail.com>
2024-09-16 12:08:06 +0200

    button fullscreen on/off

--- models/main-menu.js
+++ models/main-menu.js
@@ -1,5 +1,21 @@
-function init()
-{
-    
-    content.innerHTML += '<canvas id="canvas" width="720px" height="480px"></canvas>';
+function init() {}
+
+function fullscreenOn() {
+		const fullscreenElement = document.getElementById("content");
+		if (fullscreenElement.requestFullscreen) {
+			fullscreenElement.requestFullscreen();
+		} else if (fullscreenElement.mozRequestFullScreen) {
+			fullscreenElement.mozRequestFullScreen();
+		} else if (fullscreenElement.webkitRequestFullscreen) {
+			fullscreenElement.webkitRequestFullscreen();
+		} else if (fullscreenElement.msRequestFullscreen) {
+			fullscreenElement.msRequestFullscreen();
+		}
+        btnFullscreen.src = "assets/img/buttons/fullscreenOff.png";
+        btnFullscreen.setAttribute('onclick','fullscreenOff()');
+}
+
+function fullscreenOff(){
+    document.exitFullscreen();
+    btnFullscreen.setAttribute('onclick','fullscreenOn()');
 }
