5eb3ea2882548cab226ee161b75dc2edec6b65c3
mario.markwart <mario.markwart@gmail.com>
2024-09-27 15:27:11 +0000

    change fullscreen icon back when turn fullscreen off

--- models/main-menu.js
+++ models/main-menu.js
@@ -1,29 +1,30 @@
-function init() {}
+function init() { }
 
 function fullscreenOn() {
-		const fullscreenElement = document.getElementById("content");
-		if (fullscreenElement.requestFullscreen) {
-			fullscreenElement.requestFullscreen();
-		} else if (fullscreenElement.mozRequestFullScreen) {
-			fullscreenElement.mozRequestFullScreen();
-		} else if (fullscreenElement.webkitRequestFullscreen) {
-			fullscreenElement.webkitRequestFullscreen();
-		} else if (fullscreenElement.msRequestFullscreen) {
-			fullscreenElement.msRequestFullscreen();
-		}
-        btnFullscreen.src = "assets/img/buttons/fullscreenOff.png";
-        btnFullscreen.setAttribute('onclick','fullscreenOff()');
+	const fullscreenElement = document.getElementById("content");
+	if (fullscreenElement.requestFullscreen) {
+		fullscreenElement.requestFullscreen();
+	} else if (fullscreenElement.mozRequestFullScreen) {
+		fullscreenElement.mozRequestFullScreen();
+	} else if (fullscreenElement.webkitRequestFullscreen) {
+		fullscreenElement.webkitRequestFullscreen();
+	} else if (fullscreenElement.msRequestFullscreen) {
+		fullscreenElement.msRequestFullscreen();
+	}
+	btnFullscreen.src = "assets/img/buttons/fullscreenOff.png";
+	btnFullscreen.setAttribute('onclick', 'fullscreenOff()');
 }
 
-function fullscreenOff(){
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      }
-    btnFullscreen.setAttribute('onclick','fullscreenOn()');
+function fullscreenOff() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen();
+	} else if (document.mozCancelFullScreen) {
+		document.mozCancelFullScreen();
+	} else if (document.webkitExitFullscreen) {
+		document.webkitExitFullscreen();
+	} else if (document.msExitFullscreen) {
+		document.msExitFullscreen();
+	}
+	btnFullscreen.src = "assets/img/buttons/fullscreenOn.png";
+	btnFullscreen.setAttribute('onclick', 'fullscreenOn()');
 }
